8e0d99c03e921202bec97957dc6a505113bf107b
Azamat Xodjakov <axodjakov@gmail.com>
2021-11-05 23:26:47 +0500

    Read token

--- src/EskizsmsLaravel.php
+++ src/EskizsmsLaravel.php
@@ -1,6 +1,8 @@
 <?php
-
 namespace Azamatx\EskizsmsLaravel;
+
+use Illuminate\Support\Facades\Http;
+use File;
 
 class EskizsmsLaravel
 {
@@ -10,21 +12,44 @@ class EskizsmsLaravel
 
     public function __construct()
 	{
-		dd('test');
+		// $this->saveToken();
+		$this->token = $this->readToken();
+		print_r($this->token);
+	}
+
+	public function readToken()
+	{
+		if (File::exists(config_path('eskizsms.json'))) {
+			$config_file = File::get(config_path('eskizsms.json'));
+			return json_decode($config_file, true);
+		} else {
+
+		}
 	}
 
 	public function getToken()
 	{
-
+		$response = Http::post('https://notify.eskiz.uz/api/auth/login');
+		return $response;
 	}
 
 	public function updateToken()
 	{
-
+		
 	}
 
 	public function deleteToken()
 	{
 		
+	}
+
+	public function saveToken()
+	{
+		$data = json_encode([
+			'created' => date('Y-m-d H:i:s'),
+			'token' => 'test'
+		]);
+
+		$res = File::put(config_path('eskizsms.json'), $data);
 	}
 }
