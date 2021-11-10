4ae330d38e628ff56101cb03734a831b86f2c23a
Azamat Xodjakov <axodjakov@gmail.com>
2021-11-10 23:41:46 +0500

    Save config

--- src/EskizsmsLaravel.php
+++ src/EskizsmsLaravel.php
@@ -2,6 +2,7 @@
 namespace Azamatx\EskizsmsLaravel;
 
 use Illuminate\Support\Facades\Http;
+use Carbon\Carbon;
 use File;
 
 class EskizsmsLaravel
@@ -12,24 +13,44 @@ class EskizsmsLaravel
 
     public function __construct()
 	{
-		// $this->saveToken();
-		$this->token = $this->readToken();
-		print_r($this->token);
+		// $this->token = $this->readToken();
+		// $this->email = config('eskizsms-laravel.email');
+		// $this->password = config('eskizsms-laravel.password');
+		dd($this->getToken());
 	}
 
 	public function readToken()
 	{
-		if (File::exists(config_path('eskizsms.json'))) {
-			$config_file = File::get(config_path('eskizsms.json'));
-			return json_decode($config_file, true);
-		} else {
+		$currentTime = Carbon::now();
+		$token = '';
 
+		$token = $this->getToken();
+
+		if(File::exists(config_path('eskizsms.json'))) {
+			$config_file = File::get(config_path('eskizsms.json'));
+			$conf = json_decode($config_file, true);
+			// need to refresh token every 30 days
+			if ($currentTime->diffInDays($conf['created']) < 27) {
+				$token = $conf['token'];
+			} else {
+				$token = $this->updateToken();
+			}
+		} else {
+			$token = $this->getToken();
 		}
+
+		return $token;
 	}
 
 	public function getToken()
 	{
-		$response = Http::post('https://notify.eskiz.uz/api/auth/login');
+		$response = Http::post('https://notify.eskiz.uz/api/auth/login', [
+			'email' => $this->email,
+			'password' => $this->password,
+		]);
+
+		//$this->saveToken($response);
+
 		return $response;
 	}
 
@@ -43,11 +64,11 @@ class EskizsmsLaravel
 		
 	}
 
-	public function saveToken()
+	public function saveToken($token)
 	{
 		$data = json_encode([
 			'created' => date('Y-m-d H:i:s'),
-			'token' => 'test'
+			'token' => $token
 		]);
 
 		$res = File::put(config_path('eskizsms.json'), $data);
